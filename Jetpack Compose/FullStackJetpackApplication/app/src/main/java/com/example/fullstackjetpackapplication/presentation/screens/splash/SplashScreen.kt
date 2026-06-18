1f9c8459c02e1788a76c7f88cecb97d8c69438ee
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-06-18 17:11:28 +0530

    Added the Splash Screen

--- Jetpack Compose/FullStackJetpackApplication/app/src/main/java/com/example/fullstackjetpackapplication/presentation/screens/splash/SplashScreen.kt
+++ Jetpack Compose/FullStackJetpackApplication/app/src/main/java/com/example/fullstackjetpackapplication/presentation/screens/splash/SplashScreen.kt
@@ -1,4 +1,48 @@
 package com.example.fullstackjetpackapplication.presentation.screens.splash
 
-class SplashScreen {
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    navController: NavController
+) {
+
+    LaunchedEffect(Unit) {
+
+        delay(2000)
+
+        navController.navigate("login") {
+            popUpTo("splash") {
+                inclusive = true
+            }
+        }
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                text = "Full Stack App",
+                fontSize = 32.sp
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            CircularProgressIndicator()
+        }
+    }
 }
