e2048d98a67ebb13ab212a0ce0837cfc04bbe568
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-06-18 17:12:23 +0530

    Added the login screen

--- Jetpack Compose/FullStackJetpackApplication/app/src/main/java/com/example/fullstackjetpackapplication/presentation/screens/login/LoginScreen.kt
+++ Jetpack Compose/FullStackJetpackApplication/app/src/main/java/com/example/fullstackjetpackapplication/presentation/screens/login/LoginScreen.kt
@@ -1,4 +1,77 @@
 package com.example.fullstackjetpackapplication.presentation.screens.login
 
-class LoginScreen {
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun LoginScreen(
+    navController: NavController
+) {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "Login",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text("Email")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text("Password")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("home")
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Login")
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        TextButton(
+            onClick = {
+                navController.navigate("register")
+            }
+        ) {
+            Text("Create Account")
+        }
+    }
 }
