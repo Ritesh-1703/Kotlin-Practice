44acab4ce3c50625e00c7f53f7963c6f49424e42
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-06-18 17:15:59 +0530

    Create simple home screen

--- Jetpack Compose/FullStackJetpackApplication/app/src/main/java/com/example/fullstackjetpackapplication/presentation/screens/home/HomeScreen.kt
+++ Jetpack Compose/FullStackJetpackApplication/app/src/main/java/com/example/fullstackjetpackapplication/presentation/screens/home/HomeScreen.kt
@@ -1,4 +1,23 @@
 package com.example.fullstackjetpackapplication.presentation.screens.home
 
-class HomeScreen {
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen() {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Text(
+            text = "Welcome Home 🎉",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
 }
