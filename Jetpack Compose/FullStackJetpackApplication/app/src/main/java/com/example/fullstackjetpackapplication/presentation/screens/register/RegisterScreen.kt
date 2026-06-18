92428f70a99d4a1c52717b241efeb6b33cbf31bf
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-06-18 17:15:13 +0530

    Added the Registration screen

--- Jetpack Compose/FullStackJetpackApplication/app/src/main/java/com/example/fullstackjetpackapplication/presentation/screens/register/RegisterScreen.kt
+++ Jetpack Compose/FullStackJetpackApplication/app/src/main/java/com/example/fullstackjetpackapplication/presentation/screens/register/RegisterScreen.kt
@@ -1,4 +1,75 @@
 package com.example.fullstackjetpackapplication.presentation.screens.register
 
-class RegisterScreen {
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun RegisterScreen(
+    navController: NavController
+) {
+
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "Register",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = name,
+            onValueChange = {
+                name = it
+            },
+            label = {
+                Text("Name")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text("Email")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("home")
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Register")
+        }
+
+        TextButton(
+            onClick = {
+                navController.popBackStack()
+            }
+        ) {
+            Text("Already Have Account?")
+        }
+    }
 }
